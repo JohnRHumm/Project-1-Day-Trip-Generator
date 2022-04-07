0ff1e332acf2ccf5bcd8f082f9b78dca1aec3ca0
Humm <jhumm@mcw.edu>
2022-04-07 17:01:50 -0500

    Updated main.py with 3 cities - restaurants and activites - and travel types to start main coding. Add other cities later

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 # (10 points): As a user, I want to display my completed trip in the console. 
 # (5 points):  As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
-
+# Functions for activities and restaurants for individual cities
 def atlanta_activities():
     atlanta_activities_list = ["Falcons' game", 'Marin Luther King Jr. National Historical Park','Georgia Aquarium',\
         'Atlanta Botanical Garden','World of Coca-Cola','High Museum of Art', 'Center for Puperty Arts',\
@@ -23,10 +23,40 @@ def atlanta_restaurants():
         'Bacchanalia','Boccalupo', "B's Cracklin BBQ"]
     return atlanta_restaurant_list
     
+def boston_activities():
+    boston_activities_list = ['Red Sox game', "Patriots' game",'Boston Marathon',\
+        'Boston Tea Party Ships & Museum','John F. Kennedy Presidential Museum and Library',\
+            'Museum of Science', 'Isabela Steward Gardner Museum','Samuel Adams Brewery',\
+            'New England Aquarium', 'Castle Island']
+    return  boston_activities_list
+
+def boston_restaurants():
+    boston_restaurant_list = ['Mooncusser','Nighshade Noodle Bar','Sarma','Geppetto','Kava Neo-Taverna','Urban Health',\
+        'Barra','Cafe Sushi','Celeste','Bar Volpe']
+    return boston_restaurant_list
+
+def chicago_activities():
+    chicago_activities_list = ["Cubs' game", "Bears' game",'Willis Tower',\
+        'Magnificient Mile','The Bean','Shedd Aquarium','Field Museum',\
+            'Adler Planetarium', 'Lincoln Park Zoo','Museum of Scinece and Industry']
+    return  chicago_activities_list
+
+def chicago_restaurants():
+    chicago_restaurant_list = ['Superdawg Drive-In','Lost Larson',"Luella's Southern Kitchen",'Smoque BBQ','Lula Cafe',\
+        "Johnnie's Beef",'Porto','Lardon','Naudi Signature Pizza']
+    return chicago_restaurant_list
+
+# Define city list and methods of travel
+city_list = {'Atlanta','Boston','Chicago'}
+
+type_of_travel_list = {'Uber','Rental Car','Train','Skateboard','Bicycle','Segway',\
+    'Foot','Electric Scooter','Rickshaw','Rollerblades'}
 
 
-activities_list = atlanta_activities
+
+
+activities_list = atlanta_activities()
 print(activities_list)
 
-restaurant_list = atlanta_restaurants
+restaurant_list = atlanta_restaurants()
 print(restaurant_list)
